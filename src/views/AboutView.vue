cad66e15cea686e5454c7663972b55b89a608bb1
Zero_kun <zubairmatthee262@gmail.com>
2024-03-17 18:22:42 +0200

    Prototype

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -22,7 +22,7 @@
         </div>
         <div class="about-owner">
           <p>
-            <b>Sacry Design “African Handmade Collection”</b> is a combination of Art, art crafts design; African handmade home accessories décor; African fashion creation founded by Sarah Kahuka in 2023 inspired by African Styles, cultures and traditional Arts. Sarah K. is a “people person" with passion for Art and fashion. Originally from D.R.Congo residing in New York USA, Sarah K is very optimist and ahead of the curve who believes in staying ahead of the latest trends in fashion in combined with an authentic African piece of art that revitalized the beauty of “African-American “. Sacry Design utilizes authentic modern African art, fashion, home decor, jewelry and textiles products made by ourselves and outsource from African artisans who are proud to share African culture and heritage with the world.
+            <b>Sacry Design “African Handmade Collection”</b> is a combination of Art, art crafts design; African handmade home accessories décor; African fashion creation founded by Sarah Kahuka in 2023 inspired by African Styles, cultures and traditional Arts. Sarah K. is a “people person" with passion for Art and fashion. Originally from D.R.Congo residing in New York USA, Sarah K is very optimist and ahead of the curve who believes in staying ahead of the latest trends in fashion combined with an authentic African piece of art that revitalized the beauty of “African-American “. Sacry Design utilizes authentic modern African art, fashion, home decor, jewelry and textiles products made by ourselves and outsource from African artisans who are proud to share African culture and heritage with the world.
           </p>
           <p>
             We proudly feature the beautiful Congolese traditional cultural styles using materials directly from African Artisans. The sale of every product directly benefits the artisans who created it, which in return helps many artisans especially women in Africa in providing the essentials for their families and communities.
@@ -43,7 +43,7 @@
               Sarah K. is an enthusiastic female driven by her passion for Art and Fashion with a desire to see as many as possible adding a piece of African art to their homes and African Fashion outfit in their wardrobes. We hope you will find these products a wonderful addition to your home and collection.
             </p>
             <p>
-              Sacry Design also proudly advocates women entrepreneurship by the use of eco-friendly African materials, and how it involves fellow entrepreneurs. It is the founder’s wishes to leave the legacy and engrave own signature for a prosperous future. A huge thank you to all who support, encourage and promote this woman-owned business. The growth and success of Sacry Design is attributed to our supporters, consumers, creators, producers, sales agents.
+              Sacry Design also proudly advocates women entrepreneurship by using eco-friendly African materials, and how it involves fellow entrepreneurs. It is the founder’s wishes to leave the legacy and engrave own signature for a prosperous future. A huge thank you to all who support, encourage and promote this woman-owned business. The growth and success of Sacry Design is attributed to our supporters, consumers, creators, producers, sales agents.
             </p>
           </div>
         </div>
