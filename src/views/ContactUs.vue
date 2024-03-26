d634a7a4388bc91a8bbf914cccc360843c8c51f0
Zero_kun <zubairmatthee262@gmail.com>
2024-03-26 12:44:42 +0200

    prototype

--- src/views/ContactUs.vue
+++ src/views/ContactUs.vue
@@ -11,9 +11,9 @@
         <p><strong>Name:</strong> Sarah Kahuka</p>
         <p><strong>Address:</strong> 4804 NORSTAR BLVD APT 228 LIVERPOOL NY 13088-4286</p>
         <p><strong>Phone:</strong> +1(315)9916904</p>
-        <p><strong>Email:</strong> katalay_linon@yahoo.com</p>
-                <p><strong>Second Email:</strong> sarah@sacrydesigns.com</p>
-                        <p><strong>Third Email</strong> info@sacrydesigns.com</p>
+       <p><strong>Email:</strong><a href="mailto:sarah@sacrydesigns.com">sarah@sacrydesigns.com</a></p>
+        <p><strong>Second Email:</strong><a href="mailto:info@sacrydesigns.com">sarah@sacrydesigns.com</a></p>
+=                       
 
 
       </div>
